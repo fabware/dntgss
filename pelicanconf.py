acd163f3b04a390bfb0d7e4d881a15f9ab2342b2
fabware <xiang.can@gmail.com>
2012-10-20 14:18:54 +0800

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"向灿"
+SITENAME = u"莫猜，Be accurate!"
+SITEURL = ''
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = 'zh'
+DISQUS_SITENAME = 'dntgss'
+
+# Blogroll
+LINKS =  ((u'快玩游戏', 'http://www.kuaiwan.com'),
+          ('DBA notes', 'http://www.dbanotes.net'),
+          ('coding now', 'http://www.codingnow.cn'),
+         )
+
+# Social widget
+SOCIAL = ((u'我的微博', 'http://www.weibo.com/xiangcan'),
+          )
+
+DEFAULT_PAGINATION = 10
+
+PDF_GENERATOR = False 
+STATIC_PATHS = ["images"]
+
+
